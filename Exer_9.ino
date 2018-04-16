c47121e5ae0adc3218582b79720a254e77024b86
Vie Albastro <35793702+viejoy@users.noreply.github.com>
2018-04-17 02:12:28 +0800

    Light Bulb with Relay
    Receives input form user to turn the light bulb on/off.

---
+++ Exer_9.ino
@@ -0,0 +1,21 @@
+const int relayPin = 14;
+
+void setup() {
+Serial.begin(9600);
+pinMode(relayPin, OUTPUT);
+}
+
+void loop() {
+if(Serial.available()){
+    String userin=Serial.readString();
+    if(userin=="OFF"){
+      digitalWrite(relayPin, LOW);
+      Serial.println("Light is OFF");
+    }
+    else if(userin=="ON"){
+      digitalWrite(relayPin, HIGH);
+      Serial.println("Light is ON");
+    }
+  }
+
+}

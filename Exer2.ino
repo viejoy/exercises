7d220f3a6fda2861c579acf58f9b2899229ea44c
Vie Albastro <35793702+viejoy@users.noreply.github.com>
2018-04-17 02:03:22 +0800

    DC Motor
    Receives input from user to modulate the speed of the motor.

---
+++ Exer2.ino
@@ -0,0 +1,34 @@
+const int motorPin =9;
+int maxspeed=255;
+int midspeed=200;
+int slowspeed=180;;
+
+void setup() {
+Serial.begin(9600);
+pinMode(motorPin, OUTPUT);
+
+}
+
+void loop() {
+  if(Serial.available()){
+    char userin=Serial.read();
+    if(userin=='3'){
+      analogWrite(motorPin, maxspeed);
+      Serial.println("maxspeed");
+    }
+    else if(userin=='2'){
+      analogWrite(motorPin, midspeed);
+      Serial.println("midspeed");
+    }
+    else if(userin=='1'){
+      analogWrite(motorPin, slowspeed); 
+      Serial.println("slowspeed");
+    }
+    else if(userin=='0'){
+      analogWrite(motorPin, 0);
+      Serial.println("off");  
+    }
+   }
+}
+      
+
